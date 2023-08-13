aaeb68c7769ead0b274a196da194fa0ce474cf2e
narges <nargesvahedy0310@gmail.com>
2023-08-13 16:49:33 +0330

    create model book

---
+++ books/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_date = models.DateField()
+    page_count = models.IntegerField()
+    language = models.CharField(max_length=255)
+    available = models.BooleanField(default=True)
+    checked_out_at = models.DateTimeField(null=True, blank=True)
+    checked_out_by = models.ForeignKey('User', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
