9605679d8bbb7b26d7f8532930a1aa81e8d6b220
narges <nargesvahedy0310@gmail.com>
2023-08-14 16:50:42 +0330

    delete and change project

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,5 @@
 
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'books',
-    'users',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +119,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
