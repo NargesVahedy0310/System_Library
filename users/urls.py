f741f7e248cf6273fd159af43c3d866b19a86767
narges <nargesvahedy0310@gmail.com>
2023-08-14 15:21:03 +0330

    add serializers and urls

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import CreateUser, BorrowedBook
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CreateUser
+        fields = ['id', 'username', 'first_name', 'last_name']
+
+class BorrowedBookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BorrowedBook
+        fields = ['id', 'user', 'book', 'borrowed_at', 'returned_at']
